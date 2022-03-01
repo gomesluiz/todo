e74c69ae16fde73507ae1cc891582f2b760b8788
gomesluiz <gomes.luiz@gmail.com>
2022-03-01 11:05:36 -0300

    app.py refatorado para usar templates

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>ToDo</h1>'
+    return render_template('index.html')
