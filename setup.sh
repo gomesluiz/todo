cd232f5d7d0c1410651960bd4d97c47c9413e984
Luiz <gomes.luiz@gmail.com>
2022-03-20 18:25:57 -0300

    includes user forms interface

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 # setup git devoloper settings.
-git config --global user.email gomes.luiz@gmail.com
-git config --global user.name Luiz Gomes
+# git config --global user.email gomes.luiz@gmail.com
+# git config --global user.name Luiz Gomes
 
 # setup python virtual enviroments.
+python -m venv venv
 source venv/bin/activate
+python -m pip install --upgrade pip
 pip install -r requirements.txt
